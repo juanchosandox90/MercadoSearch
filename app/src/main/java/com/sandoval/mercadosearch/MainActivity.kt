ebba5fe32ca714dc8377d02b33e584e63234bd57
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-08 23:58:30 -0500

    Splash Screen and Theme
    - Setup the native installSplashScreen method to start with a splash screen in the optimal way
    - Setup the colors that will be used across the app and the theme linked to those colors
    - Add missing gradle dependencies to preview the build of the layouts in code and to build in the future the unit tests and the instrumentation tests
    - Upgrade minimum android sdk from 26 to 27

--- app/src/main/java/com/sandoval/mercadosearch/MainActivity.kt
+++ app/src/main/java/com/sandoval/mercadosearch/MainActivity.kt
@@ -10,36 +10,32 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sandoval.mercadosearch.ui.theme.MercadoSearchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             MercadoSearchTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Text(text = "Hello Mercado Search!")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MercadoSearchTheme {
-        Greeting("Android")
+        Text(text = "Hello Mercado Search!")
     }
 }
